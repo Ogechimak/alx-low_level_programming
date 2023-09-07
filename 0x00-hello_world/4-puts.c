63127f241c7269645d39ba30ba6c93d00b76d934
Ogechimak <ogechimac111@gmail.com>
2023-09-07 09:48:12 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio>
+/**
+ * Main - this is a c function
+ * Return: alaways return 0
+ */
+int main (void)
+{
+puts ("\"programming is like building a multilingualpuzzle");
+return (0);
+}
