cc89049c109bb58ab38781b74208c246f957bb3a
Ogechimak <ogechimac111@gmail.com>
2023-09-12 16:23:52 -0700

    commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,18 +7,21 @@
   */
 
 int main(void)
-{
-	int i = 0;
-	long j = 1, k = 2, sum = k;
 
-	while (k + j < 4000000)
-	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
-	}
-	printf("%ld", sum);
-	return (0);
+{
+
+int i = 0;
+long j = 1, k = 2, sum = k;
+while (k + j < 4000000)
+{
+k += j;
+if (k % 2 == 0)
+sum += k;
+j = k - j;
+++i;
+}
+printf("%ld", sum);
+printf("\n");
+return (0);
+
 }
