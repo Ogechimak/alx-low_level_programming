b48165a1e2d31bb23a2715b67b9c42ddeaa2bcef
Ogechimak <ogechimac111@gmail.com>
2023-09-14 14:02:18 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size: the size of triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int a;
+	int b;
+
+	for (a = 1; a <= size; a++)
+	{
+	_putchar('');
+	}
+
+	for (b = 1; b < a; b++)
+	{
+	_putchar('#');
+	}
+	}
+}
