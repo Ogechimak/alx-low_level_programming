acd78f2f12c101d5a10ec2a1020b97837bf214da
Ogechimak <ogechimac111@gmail.com>
2023-09-14 08:38:28 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase
+ * @c: uppercase character
+ * Return: return 1 if c is uppercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+	return (1);
+
+	else
+
+	return (0);
+}
