625f68e3316a6a827feeed57407c318174e6ae9f
Ogechimak <ogechimac111@gmail.com>
2023-09-14 14:15:19 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,31 +10,32 @@
 
 int main(void)
 {
-	int a;
+	int i;
 
-	for (a = 1; a <= 100; a++)
+	for (i = 1; i <= 100; i++)
 	{
-	if (a % 3 == 0 && a % 5 != 0)
+	if (i % 3 == 0 && i % 5 != 0)
 	{
-	printf("Fizz");
+	printf(" Fizz");
 	}
-	else if (a % 5 == 0 && 3 != 0)
+	else if (i % 5 == 0 && i % 3 != 0)
 	{
-	printf("Buzz");
+	printf(" Buzz");
 	}
-	else if (a % 3 == 0 && a % 5 == 0)
+	else if (i % 3 == 0 && i % 5 == 0)
 	{
-	printf("FizzBuzz");
+	printf(" FizzBuzz");
 	}
-	else if (a == 1)
+	else if (i == 1)
 	{
-	printf("%d", a);
+	printf("%d", i);
 	}
 	else
 	{
-	printf("%d", a);
+	printf(" %d", i);
 	}
 	}
 	printf("\n");
+
 	return (0);
 }
