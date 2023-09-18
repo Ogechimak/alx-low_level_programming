31e6f34fa6d30ca04d36b8be1a4b847c9000235c
Ogechimak <ogechimac111@gmail.com>
2023-09-18 13:31:14 -0700

    commit changes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifdef MAIN_H
-#define MAIN_H
+
 #include <stdio.h>
 
 int _putchar(char c);
@@ -14,4 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
+
