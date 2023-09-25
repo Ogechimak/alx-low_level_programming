63f63789304969e67631ebd15af2e548d57dba55
Ogechimak <ogechimac111@gmail.com>
2023-09-25 10:05:09 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for anny of a set of bytes
+ * @s: data 1
+ * @accept: data 2
+ * Return: return s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		const char *a = accept;
+	while (*a != '\0')
+	{
+		if (*s == *a)
+		return (s);
+		a++;
+	}
+	s++;
+	}
+	return (NULL);
+}
