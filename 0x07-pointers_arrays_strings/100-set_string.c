6acf2a452b63109da4e8ea57ff9fc4ccbf5269b3
Ogechimak <ogechimac111@gmail.com>
2023-09-25 11:39:40 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function that set the value of a pointer
+ * @s: data 1
+ * @to: data 2
+ * Return: return 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
