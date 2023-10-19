2411e37e3c3d760a5ebdffb960413b9fb99f1071
Ogechimak <ogechimac111@gmail.com>
2023-10-19 06:44:31 -0700

    commit 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * list_len - function that returns the number of elements
+ * @h: the pointer
+ * Return: return the num of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+
+	size_t n = 0;
+
+
+	while (h)
+
+	{
+
+	n++;
+	h = h->next;
+
+	}
+	return (n);
+
+}
