66b0f1964bd85cb951b3693fb8b55afe43996a73
Ogechimak <ogechimac111@gmail.com>
2023-10-30 15:14:30 -0700

    commit 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends text
+ * @filename: the file name
+ * @text_content: this is a string terminated by NULL
+ * Return: return 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, mop;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
+	else
+	i = 0;
+
+	mop = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	write(mop, text_content, i);
+
+	if (mop == -1)
+		return (-1);
+	close(mop);
+	return (1);
+}
